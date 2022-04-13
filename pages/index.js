5251e544ac6cdcae8832c79ab7488944d33dc186
bruce.slaughter <bruce@ns.expert>
2022-04-13 12:44:53 -0400

    added ggogle anal

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const HomePage7 =() => {
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-4BJLTD0RQT"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-4BJLTD0RQT');
